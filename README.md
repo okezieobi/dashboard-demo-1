2129eb1b7b73f1460c437d7deed58f6779fc22b5
Frank Okezie Obiedere <obiedereokezie@gmail.com>
2021-02-22 12:30:38 +0100

    Bakcground set

--- README.md
+++ README.md
@@ -1,12 +1,4 @@
-# react-template
-
-React  template
-
-[![Build Status](https://travis-ci.org/okezieobi/react-template.svg?branch=main)](https://travis-ci.org/okezieobi/react-template)
-
-[![Coverage Status](https://coveralls.io/repos/github/okezieobi/react-template/badge.svg?branch=main)](https://coveralls.io/github/okezieobi/react-template?branch=main)
-
-[![Netlify Status](https://api.netlify.com/api/v1/badges/08bea14d-d760-484f-906f-ce9ef235a287/deploy-status)](https://app.netlify.com/sites/my-custom-react-template/deploys)
+# dashboard demo
 
 ## Getting Started with Create React App
 
--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,118 @@
+/* eslint-disable react/forbid-prop-types */
+/* eslint-disable react/require-default-props */
+/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 
-function App() {
+function TabPanel(props) {
+  const {
+    children, value, index, ...other
+  } = props;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`vertical-tabpanel-${index}`}
+      aria-labelledby={`vertical-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
     </div>
   );
 }
 
-export default App;
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `vertical-tab-${index}`,
+    'aria-controls': `vertical-tabpanel-${index}`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+    display: 'flex',
+    height: '100vh',
+    borderRadius: theme.spacing(2),
+  },
+  tabs: {
+    borderRight: `1px solid ${theme.palette.divider}`,
+    paddingTop: theme.spacing(6),
+  },
+  body: {
+    padding: theme.spacing(6),
+    backgroundColor: '#E5E5E5',
+  },
+  paper: {
+    borderRadius: '10%',
+  },
+}));
+
+export default function App() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className={classes.body}>
+      <div className={classes.root}>
+        <Tabs
+          orientation="vertical"
+          value={value}
+          onChange={handleChange}
+          aria-label="Vertical tabs example"
+          className={classes.tabs}
+        >
+          <Tab label="Item One" {...a11yProps(0)} />
+          <Tab label="Item Two" {...a11yProps(1)} />
+          <Tab label="Item Three" {...a11yProps(2)} />
+          <Tab label="Item Four" {...a11yProps(3)} />
+          <Tab label="Item Five" {...a11yProps(4)} />
+          <Tab label="Item Six" {...a11yProps(5)} />
+          <Tab label="Item Seven" {...a11yProps(6)} />
+        </Tabs>
+        <TabPanel value={value} index={0}>
+          Item One
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          Item Two
+        </TabPanel>
+        <TabPanel value={value} index={2}>
+          Item Three
+        </TabPanel>
+        <TabPanel value={value} index={3}>
+          Item Four
+        </TabPanel>
+        <TabPanel value={value} index={4}>
+          Item Five
+        </TabPanel>
+        <TabPanel value={value} index={5}>
+          Item Six
+        </TabPanel>
+        <TabPanel value={value} index={6}>
+          Item Seven
+        </TabPanel>
+      </div>
+    </div>
+  );
+}
