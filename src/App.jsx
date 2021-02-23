fca0124fbc2158c23aa9e174199ad3e8fb6a515e
Frank Okezie Obiedere <obiedereokezie@gmail.com>
2021-02-23 13:59:42 +0100

    Latest commits

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,17 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
+import HomeIcon from '@material-ui/icons/Home';
+import Toolbar from '@material-ui/core/Toolbar';
+import PeopleIcon from '@material-ui/icons/People';
+import CardIcon from '@material-ui/icons/CardMembership';
+import MessageIcon from '@material-ui/icons/MessageRounded';
+import SettingsIcon from '@material-ui/icons/Settings';
 
 function TabPanel(props) {
   const {
@@ -44,24 +50,47 @@ function a11yProps(index) {
   };
 }
 
+const StyledTabs = withStyles({
+  indicator: {
+    display: 'flex',
+    backgroundColor: 'transparent',
+    '& > span': {
+      width: '100%',
+      backgroundColor: '#635ee7',
+    },
+  },
+})((props) => <Tabs {...props} TabIndicatorProps={{ children: <span /> }} />);
+
+const StyledTab = withStyles((theme) => ({
+  root: {
+    textTransform: 'none',
+    color: '#2979F2',
+    fontWeight: theme.typography.fontWeightBold,
+    fontSize: theme.typography.pxToRem(15),
+    marginRight: theme.spacing(1),
+    '&:focus': {
+      opacity: 1,
+    },
+  },
+}))((props) => <Tab disableRipple {...props} />);
+
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
     backgroundColor: theme.palette.background.paper,
     display: 'flex',
     height: '100vh',
-    borderRadius: theme.spacing(2),
   },
   tabs: {
-    borderRight: `1px solid ${theme.palette.divider}`,
+    borderRight: '1px solid #2979F2',
     paddingTop: theme.spacing(6),
+    backgroundColor: '#F4F5FA',
   },
-  body: {
-    padding: theme.spacing(6),
-    backgroundColor: '#E5E5E5',
+  tabIcon: {
+    marginRight: theme.spacing(1),
   },
-  paper: {
-    borderRadius: '10%',
+  lastTab: {
+    flexGrow: 1,
   },
 }));
 
@@ -74,45 +103,77 @@ export default function App() {
   };
 
   return (
-    <div className={classes.body}>
-      <div className={classes.root}>
-        <Tabs
-          orientation="vertical"
-          value={value}
-          onChange={handleChange}
-          aria-label="Vertical tabs example"
-          className={classes.tabs}
-        >
-          <Tab label="Item One" {...a11yProps(0)} />
-          <Tab label="Item Two" {...a11yProps(1)} />
-          <Tab label="Item Three" {...a11yProps(2)} />
-          <Tab label="Item Four" {...a11yProps(3)} />
-          <Tab label="Item Five" {...a11yProps(4)} />
-          <Tab label="Item Six" {...a11yProps(5)} />
-          <Tab label="Item Seven" {...a11yProps(6)} />
-        </Tabs>
-        <TabPanel value={value} index={0}>
-          Item One
-        </TabPanel>
-        <TabPanel value={value} index={1}>
-          Item Two
-        </TabPanel>
-        <TabPanel value={value} index={2}>
-          Item Three
-        </TabPanel>
-        <TabPanel value={value} index={3}>
-          Item Four
-        </TabPanel>
-        <TabPanel value={value} index={4}>
-          Item Five
-        </TabPanel>
-        <TabPanel value={value} index={5}>
-          Item Six
-        </TabPanel>
-        <TabPanel value={value} index={6}>
-          Item Seven
-        </TabPanel>
-      </div>
+    <div className={classes.root}>
+      <StyledTabs
+        orientation="vertical"
+        value={value}
+        onChange={handleChange}
+        aria-label="Vertical tabs"
+        className={classes.tabs}
+        variant="fullWidth"
+      >
+        <StyledTab
+          label={(
+            <Toolbar>
+              <HomeIcon className={classes.tabIcon} />
+              Dashboard
+            </Toolbar>
+)}
+          {...a11yProps(0)}
+        />
+        <StyledTab
+          label={(
+            <Toolbar>
+              <PeopleIcon className={classes.tabIcon} />
+              Companies
+            </Toolbar>
+)}
+          {...a11yProps(1)}
+        />
+        <StyledTab
+          label={(
+            <Toolbar>
+              <CardIcon className={classes.tabIcon} />
+              Projects
+            </Toolbar>
+)}
+          {...a11yProps(2)}
+        />
+        <StyledTab
+          label={(
+            <Toolbar>
+              <MessageIcon className={classes.tabIcon} />
+              Messages
+            </Toolbar>
+)}
+          {...a11yProps(3)}
+        />
+        <StyledTab
+          className={classes.lastTab}
+          label={(
+            <Toolbar>
+              <SettingsIcon className={classes.tabIcon} />
+              Settings
+            </Toolbar>
+)}
+          {...a11yProps(4)}
+        />
+      </StyledTabs>
+      <TabPanel value={value} index={0}>
+        Dashboard
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Companies
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        Projects
+      </TabPanel>
+      <TabPanel value={value} index={3}>
+        Messages
+      </TabPanel>
+      <TabPanel value={value} index={4}>
+        Settings
+      </TabPanel>
     </div>
   );
 }
